b95e54c5ab8f239817d41520f47e48544cc75d1f
Abdulazeem000 <49524320+Abdulazeem000@users.noreply.github.com>
2019-04-11 18:36:29 +0500

    Add files via upload

---
+++ Lab 1/TaskTwo.java
@@ -0,0 +1,14 @@
+class TaskTwo
+{
+  public static void main(String args[])
+   {
+    String a = args[0];
+    String b = args[1];
+    String c = args[2];
+    double    d =Double.parseDouble(a);
+    double    e =Double.parseDouble(b);
+    double    f =Double.parseDouble(c);
+
+    System.out.println(d+e+f);
+   }
+} 
